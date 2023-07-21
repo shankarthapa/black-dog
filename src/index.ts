625c6a9c6cf88d0920a03d0289bc44b759153dd4
Shankar Thapa <shankasthapa@gmail.com>
2023-07-21 19:53:20 +0530

    test 5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export * from './components';
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'my-component';
+
+
+
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,13 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'black-dog',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: 'my-component',
+      proxiesFile: './src/index.ts'
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
